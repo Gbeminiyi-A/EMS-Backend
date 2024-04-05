e7e5a5f300cfad0bd5aeee3f08dbbfd3d569b31c
Oluwagbeminiyi Agbedejobi <gagbedejobi@gmail.com>
2024-04-05 09:12:09 +0100

    added the cors_headers list

--- backend/settings.py
+++ backend/settings.py
@@ -67,7 +67,19 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:3030',
+    'http://localhost:3000',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+)
 
 CORS_ALLOW_METHODS = (
     "DELETE",
